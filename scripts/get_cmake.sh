50a12a23cfd9fe7fde29713934c84e4afb0acb31
tsky <thomas.kollakowsky@w-hs.de>
2025-06-19 11:45:57 +0200

    4.0.3

--- scripts/get_cmake.sh
+++ scripts/get_cmake.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-cmake_version=4.0.2
+cmake_version=4.0.3
 
 wget https://github.com/Kitware/CMake/releases/download/v$cmake_version/cmake-$cmake_version.tar.gz
 tar xfz cmake-$cmake_version.tar.gz
