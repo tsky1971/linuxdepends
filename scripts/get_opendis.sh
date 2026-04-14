3194014e02a6d4167602cb4c0bd0be7abe34eb23
thomas.kollakowsky <thomas.kollakowsky@w-hs.de>
2026-04-14 09:43:09 +0200

    update

--- scripts/get_opendis.sh
+++ scripts/get_opendis.sh
@@ -6,18 +6,53 @@ if [ ! -d OpenDiS ]
 then 
 	git clone --recurse-submodule https://github.com/OpenDiS/OpenDiS.git
 	cd OpenDiS
+		cd core/pydis
+			mkdir build
+			cd build
+				cmake ..
+				make
+				sudo make install
+			cd ..
+		cd ..
+		cd core/exadis
+			mkdir build
+			cd build
+				cmake ..
+				make
+				sudo make install
+			cd ..
+		cd ..
 		mkdir build
 		cd build
 			cmake ..
 			make
 			sudo make install	
-		cd ..
+		cd ..		
 	cd ..	
 else
 	echo "OpenDiS exists already"
 	cd OpenDiS
 		git pull
 		git submodule update --init --recursive
+		
+		cd core/pydis
+			mkdir build
+			cd build
+				cmake ..
+				make
+				sudo make install
+			cd ..
+		cd ..
+		
+		cd core/exadis
+			mkdir build
+			cd build
+				cmake ..
+				make
+				sudo make install
+			cd ..
+		cd ..
+		
 		cd build
 			cmake ..
 			make
